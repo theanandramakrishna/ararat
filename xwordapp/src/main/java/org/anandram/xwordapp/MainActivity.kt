c3ab39f1d328f120ba120e0328c62d2c802c452e
theanandramakrishna <theanandramakrishna@gmail.com>
2025-12-31 11:43:04 -0800

    rename app (#1)

--- xwordapp/src/main/java/org/anandram/xwordapp/MainActivity.kt
+++ xwordapp/src/main/java/org/anandram/xwordapp/MainActivity.kt
@@ -18,7 +18,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-package org.akop.crosswordtest
+package org.anandram.xwordapp
 
 import android.graphics.Shader
 import android.graphics.drawable.BitmapDrawable
